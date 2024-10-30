4f2ea3de0bf035abc2e4bc9fba114349ab89004a
Chashch-Anya <chashuhina00@gmail.com>
2024-10-30 19:58:05 +0300

    gh-pages

--- src/loaders/readData.js
+++ src/loaders/readData.js
@@ -4,23 +4,9 @@ export function ReadData() {
 
     const [data, setData] = useState([]);
 
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         try {
-    //             const response = await fetch('/quiz.json'); // Убедиться, что файл доступен
-    //             const jsonData = await response.json();
-    //             setData(jsonData.quiz.categories);
-    //         } catch (error) {
-    //             console.error('Ошибка при загрузке данных:', error);
-    //         }
-    //     };
-
-    //     fetchData();
-    // }, []);
-
     useEffect(() => {
         // Загрузка JSON файла
-        fetch('/quiz.json')
+        fetch('/quiz-app/quiz.json')
             .then((response) => response.json())
             .then((data) => {
                 setData(data.quiz.categories);
